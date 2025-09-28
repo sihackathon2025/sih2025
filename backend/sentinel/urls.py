146128b4ffc0f19faf8dba9298dd183be658e700
Abhishek yaduwanshi <abhishekyaduwanshi4852@gmail.com>
2025-09-28 19:30:11 +0530

    all fix

--- backend/sentinel/urls.py
+++ backend/sentinel/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('api/dashboard/', include('admindashboard.urls')),
     path('api/ngoData/', include('data_collection.urls')),
     path('api/users/', include('users.urls')),
-    path('api/prediction/', include('prediction.urls')),
-    path('api/dashboard/', include('admindashboard.urls')),
+   
 
 ]
 
